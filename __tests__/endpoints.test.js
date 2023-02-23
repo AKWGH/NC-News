e8cee9ecfbb8f027dccddccf476df67528fa68ff
Andrew Williams <andrew.k.w@outlook.com>
2023-02-23 16:01:45 +0000

    added test GET /api/users

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -290,4 +290,23 @@ describe('app endpoint tests', () => {
         });
     });
   });
+  describe('GET /api/users', () => {
+    it('should respond with 200 and an array of all the users in the database', () => {
+      return request(app)
+        .get('/api/users')
+        .expect(200)
+        .then(({ body }) => {
+          const { users } = body;
+          expect(users.length).toBe(4);
+
+          users.forEach((user) => {
+            expect(user).toMatchObject({
+              username: expect.any(String),
+              name: expect.any(String),
+              avatar_url: expect.any(String),
+            });
+          });
+        });
+    });
+  });
 });
